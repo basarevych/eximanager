219408eb59bd09415190ef82c5a276841fcc1e64
Ross Basarevych <basarevych@gmail.com>
2015-05-27 09:49:23 +0300

    More tests

--- test/fileManagerSpec.js
+++ test/fileManagerSpec.js
@@ -12,15 +12,29 @@ describe("FileManager", function () {
         fm = new FileManager(sl);
     });
 
+    it("counts lines", function (done) {
+        var filename = '/tmp/test.tmp';
+
+        fs.writeFile(filename, "one\ntwo\nthree\n", function(err) {
+            fm.countLines(filename)
+                .then(function (lines) {
+                    fs.unlinkSync(filename);
+                    expect(lines).toBe(3);
+                    done();
+                });
+        });
+    });
+
     it("copies file", function (done) {
-        var filename = __dirname + '/../config.js.dist';
+        var filename1 = __dirname + '/../config.js.dist',
+            filename2 = '/tmp/test.tmp';
 
-        fm.copyFile(filename, filename + '.tmp')
+        fm.copyFile(filename1, filename2)
             .then(function () {
-                var orig = fs.readFileSync(filename),
-                    copy = fs.readFileSync(filename + '.tmp');
+                var orig = fs.readFileSync(filename1),
+                    copy = fs.readFileSync(filename2);
 
-                fs.unlinkSync(filename + '.tmp');
+                fs.unlinkSync(filename2);
                 expect(copy).toEqual(orig);
                 done();
             });
