f2f79fdf8d92055151bcc15765f0d9cf06b21c6e
Ross Basarevych <basarevych@gmail.com>
2015-05-27 10:02:12 +0300

    fm.iterateDir test

--- test/fileManagerSpec.js
+++ test/fileManagerSpec.js
@@ -12,17 +12,36 @@ describe("FileManager", function () {
         fm = new FileManager(sl);
     });
 
+    it("iterates directory", function (done) {
+        var base = '/tmp/eximanager-test';
+
+        fs.mkdirSync(base);
+        fs.mkdirSync(base + '/one');
+        fs.writeFileSync(base + '/two', "one\ntwo\nthree\n");
+        fm.iterateDir(base)
+            .then(function (files) {
+                fs.unlinkSync(base + '/two');
+                fs.rmdirSync(base + '/one');
+                fs.rmdirSync(base);
+                expect(files.length).toBe(2);
+                expect(files[0].name).toBe('one');
+                expect(files[0].stats.isDirectory()).toBeTruthy();
+                expect(files[1].name).toBe('two');
+                expect(files[1].stats.isDirectory()).toBeFalsy();
+                done();
+            });
+    });
+
     it("counts lines", function (done) {
         var filename = '/tmp/test.tmp';
 
-        fs.writeFile(filename, "one\ntwo\nthree\n", function(err) {
-            fm.countLines(filename)
-                .then(function (lines) {
-                    fs.unlinkSync(filename);
-                    expect(lines).toBe(3);
-                    done();
-                });
-        });
+        fs.writeFileSync(filename, "one\ntwo\nthree\n");
+        fm.countLines(filename)
+            .then(function (lines) {
+                fs.unlinkSync(filename);
+                expect(lines).toBe(3);
+                done();
+            });
     });
 
     it("copies file", function (done) {
